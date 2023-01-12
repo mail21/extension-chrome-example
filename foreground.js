1b5851933e0e831f8e93a6ba4473f9652c3c7b22
mail21 <ismailidn1@gmail.com>
2023-01-12 14:55:29 +0700

    Test add

--- foreground.js
+++ foreground.js
@@ -4,4 +4,40 @@
 // Several foreground scripts can be declared
 // and injected into the same or different pages.
 
-console.log("This prints to the console of the page (injected only if the page url matched)")
+const main = () => {
+  console.log("content Main");
+  /**
+   * Fired when a message is sent from either an extension process or a content script.
+   */
+
+  // check url
+  //   document.body.onload = function () {
+  //     document.body.innerHTML += "<h1>HELLO</h1>";
+  //     // document.getElementById("ctl00_cplMain_btnRegister").click();
+  //     // document.getElementById("UserName").value = "Elon";
+  //     // document.getElementsByClassName("_42ft _4jy0 _6lth _4jy6 _4jy1 selected _51sy")[0].click();
+  //   };
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var activeTab = tabs[0];
+    var activeTabUrl = activeTab.url;
+    // do something with the active tab url
+    console.log(activeTabUrl, "activeTabUrl");
+  });
+
+  window.addEventListener(
+    "message",
+    (event) => {
+      //   Only accept messages from ourselves
+      if (event.source !== window) return;
+
+      if (event.data.type && event.data.type === "CLOSE_SUPPLIER_WEBSITE") {
+        // console.log("Content script received CLOSE_SUPPLIER_WEBSITE event");
+        chrome.runtime.sendMessage({ closeThis: true });
+      }
+    },
+    false
+  );
+};
+
+main();
--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,34 @@
 // when the extension is installed or refreshed (or when you access its console).
 // It would correspond to the background script in chrome extensions v2.
 
-console.log("This prints to the console of the service worker (background script)")
+console.log("This prints to the console of the service worker (background script)");
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log(sender);
+  console.log(message);
+  if (message.closeThis) {
+    CLOSE_SUPPLIER_WEBSITE(sender.tab.id);
+  }
+});
+
+const CLOSE_SUPPLIER_WEBSITE = (id) => {
+  //   chrome.tabs.remove(id);
+  console.log("run CLOSE_SUPPLIER_WEBSITE");
+  chrome.tabs.query({}, function (tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      if (
+        tabs[i].url.includes("my.api.net.au") ||
+        tabs[i].url.includes("hts.symbionhealth.com") ||
+        tabs[i].url.includes("ch2.net.au")
+      ) {
+        chrome.tabs.remove(tabs[i].id);
+      }
+    }
+  });
+};
 
 // Importing and using functionality from external files is also possible.
-importScripts('service-worker-utils.js')
+importScripts("service-worker-utils.js");
 
 // If you want to import a file that is deeper in the file hierarchy of your
 // extension, simply do `importScripts('path/to/file.js')`.
