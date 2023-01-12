7060713f72af0a1f2765e5b7aa460fa80b647037
mail21 <ismailidn1@gmail.com>
2023-01-12 19:40:25 +0700

    update

--- foreground.js
+++ foreground.js
@@ -5,7 +5,7 @@
 // and injected into the same or different pages.
 
 const main = () => {
-  console.log("content Main");
+  console.log('content Main');
   /**
    * Fired when a message is sent from either an extension process or a content script.
    */
@@ -18,20 +18,37 @@ const main = () => {
   //     // document.getElementsByClassName("_42ft _4jy0 _6lth _4jy6 _4jy1 selected _51sy")[0].click();
   //   };
 
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    var activeTab = tabs[0];
-    var activeTabUrl = activeTab.url;
-    // do something with the active tab url
-    console.log(activeTabUrl, "activeTabUrl");
+  var port = chrome.runtime.connect({ name: 'my_connection' });
+  console.log(port, 'port');
+
+  port.postMessage({ type: 'getActiveTabUrl' });
+
+  // Listen for messages from the background script
+  port.onMessage.addListener(function (message) {
+    if (message.type === 'activeTabUrl') {
+      var activeTabUrl = message.url;
+      // Do something with the active tab URL
+      console.log(activeTabUrl, 'activeTabUrl');
+      // prevent dont being able to click in the web site
+      if (!activeTabUrl.includes('ashwins.com.au')) {
+        document.body.onload = function () {
+          // symbion
+          document.getElementById('UserName').value = 'Elon';
+          document.getElementById('Password').value = 'Elon';
+          document.getElementById('RememberMe').checked = true;
+          // document.getElementById('ctl00_cplMain_Login2_LoginButton').click()
+        };
+      }
+    }
   });
 
   window.addEventListener(
-    "message",
+    'message',
     (event) => {
       //   Only accept messages from ourselves
       if (event.source !== window) return;
 
-      if (event.data.type && event.data.type === "CLOSE_SUPPLIER_WEBSITE") {
+      if (event.data.type && event.data.type === 'CLOSE_SUPPLIER_WEBSITE') {
         // console.log("Content script received CLOSE_SUPPLIER_WEBSITE event");
         chrome.runtime.sendMessage({ closeThis: true });
       }
--- service-worker.js
+++ service-worker.js
@@ -2,25 +2,44 @@
 // when the extension is installed or refreshed (or when you access its console).
 // It would correspond to the background script in chrome extensions v2.
 
-console.log("This prints to the console of the service worker (background script)");
+console.log('This prints to the console of the service worker (background script)');
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   console.log(sender);
   console.log(message);
   if (message.closeThis) {
     CLOSE_SUPPLIER_WEBSITE(sender.tab.id);
+    // chrome.tabs.create({
+    //   url: 'https://hts.symbionhealth.com/shop/',
+    //   active: false, // makes the new tab active
+    // });
+  }
+});
+
+chrome.runtime.onConnect.addListener(function (port) {
+  if (port.name === 'my_connection') {
+    // Listen for messages from the content script
+    port.onMessage.addListener(function (message) {
+      if (message.type === 'getActiveTabUrl') {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+          var activeTab = tabs[0];
+          var activeTabUrl = activeTab.url;
+          port.postMessage({ type: 'activeTabUrl', url: activeTabUrl });
+        });
+      }
+    });
   }
 });
 
 const CLOSE_SUPPLIER_WEBSITE = (id) => {
   //   chrome.tabs.remove(id);
-  console.log("run CLOSE_SUPPLIER_WEBSITE");
+  console.log('run CLOSE_SUPPLIER_WEBSITE');
   chrome.tabs.query({}, function (tabs) {
     for (var i = 0; i < tabs.length; i++) {
       if (
-        tabs[i].url.includes("my.api.net.au") ||
-        tabs[i].url.includes("hts.symbionhealth.com") ||
-        tabs[i].url.includes("ch2.net.au")
+        tabs[i].url.includes('my.api.net.au') ||
+        tabs[i].url.includes('hts.symbionhealth.com') ||
+        tabs[i].url.includes('ch2.net.au')
       ) {
         chrome.tabs.remove(tabs[i].id);
       }
@@ -29,7 +48,7 @@ const CLOSE_SUPPLIER_WEBSITE = (id) => {
 };
 
 // Importing and using functionality from external files is also possible.
-importScripts("service-worker-utils.js");
+importScripts('service-worker-utils.js');
 
 // If you want to import a file that is deeper in the file hierarchy of your
 // extension, simply do `importScripts('path/to/file.js')`.
